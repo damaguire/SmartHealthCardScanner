1437fb029ef472fb631830f1eb61499d00be3a57
damaguire <danielamaguire@gmail.com>
2021-11-02 13:27:18 -0700

    Added version number
    added a version number on the footer to help me keep track of updates

--- src/Footer.js
+++ src/Footer.js
@@ -2,20 +2,9 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
+import Box from '@material-ui/core/Box';
 
 const useStyles = makeStyles((theme) => ({
-  text: {
-    padding: theme.spacing(2, 2, 0),
-  },
-  paper: {
-    paddingBottom: 50,
-  },
-  list: {
-    marginBottom: theme.spacing(2),
-  },
-  subheader: {
-    backgroundColor: theme.palette.background.paper,
-  },
   appBar: {
     top: 'auto',
     bottom: 0,
@@ -27,26 +16,25 @@ const useStyles = makeStyles((theme) => ({
     height: 40,
     padding: '0 30px',
   },
-  grow: {
-    flexGrow: 1,
-  },
   fabButton: {
     position: 'absolute',
-    zIndex: 1,
-    top: -30,
-    left: 0,
-    right: 0,
-    margin: '0 auto',
+    flexGrow: 1
+  },
+  fabButton2: {
+    position: 'absolute',
+    flexGrow: 1,
+    marginLeft: '150px',
   },
 }));
 
 function Footer() {
   const classes = useStyles();
   return (
-    <div className={classes.root}>
-      <AppBar position="fixed" color="primary" className={classes.appBar}>
-        <Button color="inherit" href="https://github.com/damaguire/SmartHealthCardScanner">Github Repo</Button>
-      </AppBar>
+    <div>
+        <AppBar position="fixed" color="primary" className={classes.appBar}>
+          <Button color="inherit" href="https://github.com/damaguire/SmartHealthCardScanner" className={classes.fabButton}>Github Repo</Button>
+          <Button color="inherit" href="https://github.com/damaguire/SmartHealthCardScanner/commits/main" className={classes.fabButton2}>v1.3</Button>
+        </AppBar>
     </div>
   );
 }
